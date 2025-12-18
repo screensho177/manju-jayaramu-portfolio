a878363e6f31aabdcc315eeab9730af4d43b0157
screensho177 <manjujayaramu511@gmail.com>
2025-12-18 17:50:37 +0100

    contact updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Huzaif Ahmed",
+          to_name: "Manju Jayaramu",
           from_email: form.email,
-          to_email: "dev.huzaif@gmail.com",
+          to_email: "manjuj276@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
